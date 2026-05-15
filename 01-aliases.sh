3425b0d21d60554ec10bc39222e999fa0c1e7001
Antonio Ribeiro <703858+malvadeza@users.noreply.github.com>
2026-05-15 17:17:11 -0600

    feat(alias): more useful aliases

--- 01-aliases.sh
+++ 01-aliases.sh
@@ -5,7 +5,8 @@ alias zshedit='nvim ~/.zshrc'
 
 # Git Aliases
 alias gdst='git diff --stat'
+alias ggpdel='git push origin :"$(git_current_branch)"'
 alias ggpforce='ggpush --force'
 alias ggploko='ggpush --no-verify'
-alias ggpyolo='ggpforce --no-verify'
 alias ggproc='git pull --rebase origin "$(git_current_branch)"'
+alias ggpyolo='ggpforce --no-verify'
