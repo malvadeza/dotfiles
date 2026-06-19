0fa2d1438941cce1e469a1b1809f185075bd6d58
Antonio Ribeiro <703858+malvadeza@users.noreply.github.com>
2026-06-19 11:16:48 -0300

    feat(aliases): add git aliases for different remote

--- 01-aliases.sh
+++ 01-aliases.sh
@@ -4,6 +4,26 @@ alias zshsrc='source ~/.zshrc'
 alias zshedit='nvim ~/.zshrc'
 
 # Git Aliases
+
+# gpush with required remote selection
+gpush() {
+  local remote="$1"
+  shift
+  git push "$remote" "$(git_current_branch)" "$@"
+}
+
+gpforce() {
+  gpush "$1" --force
+}
+
+gploko() {
+  gpush "$1" --no-verify
+}
+
+gpyolo() {
+  gpush "$1" --force --no-verify
+}
+
 alias gdst='git diff --stat'
 alias ggpdel='git push origin :"$(git_current_branch)"'
 alias ggpforce='ggpush --force'
